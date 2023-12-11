ef2000ded2f5635f1db8d6e2d7fc7f2d89248f10
Yash Gorasia <y.t.gorasia@gmail.com>
2023-12-11 15:14:05 +0530

    FAQs section styling done

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const splide = new Splide(".splide", {
   type: "loop",
 });
 splide.mount();
+
+const faqs = document.querySelectorAll(".faqs__item");
+
+faqs.forEach((faq) => {
+  faq.querySelector(".faqs__question").addEventListener("click", () => {
+    faq.setAttribute("aria-expanded", faq.ariaExpanded === "true" ? "false" : "true");
+  });
+});
